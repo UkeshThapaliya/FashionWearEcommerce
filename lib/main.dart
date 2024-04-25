04d48408ef1a79c1fe662ec34f973846042ea1d8
UT404 <ukeshthapaliya10@gmail.com>
2024-04-25 23:09:41 +0545

    integrate Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,21 @@
+import 'dart:io';
+
 import 'package:fashionwear_ecommerce/views/buyers/main_screen.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  Platform.isAndroid
+      ? await Firebase.initializeApp(
+          options: FirebaseOptions(
+              apiKey: "AIzaSyDN_R7TovMvsysXZ1naGx69VC_NuDPjv4Y",
+              appId: "1:245683165794:android:c5480e46049bf6b43a8f3b",
+              messagingSenderId: "245683165794 ",
+              projectId: "fashionwear-9e6ab",
+              storageBucket: "gs://fashionwear-9e6ab.appspot.com"))
+      : await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
